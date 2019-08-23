0a9220435992a3bdef3e93fbe0c061ac256d25eb
Ryan <noreply>
2019-08-23 11:28:58 +0800

    Level 4. Todos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -19,35 +18,57 @@ public class Duke {
     private static void chatbot() {
         boolean exitCondition = false;
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> list = new ArrayList<>();
+        Task[] list = new Task[100];
         int items = 0;
 
         while (!exitCondition) {
             String string = scanner.nextLine();
-            System.out.println("________________________________________");
+            StringTokenizer tokenizer = new StringTokenizer(string);
+            String command = tokenizer.nextToken();
+            String description = null;
+            String date = null;
+            if (tokenizer.hasMoreTokens()) description = tokenizer.nextToken("/\n");
+            if (tokenizer.hasMoreTokens()) date = tokenizer.nextToken("\n");
 
-            if (string.equals("bye")) {
-                exitCondition = true;
-                System.out.println("    Bye. Hope to see you again soon!");
-            } else if (string.equals("list")) {
-                System.out.println("    Here are the tasks in your list:");
-                for (int i = 0; i < items; i++) {
-                    System.out.println("    "+ (i+1) + ". [" + list.get(i).getStatusIcon()+"] "
-                            + list.get(i).description);
-                }
-            } else {
-                StringTokenizer tokenizer = new StringTokenizer(string);
-                if (tokenizer.nextToken().equals("done")) {
-                    int item = Integer.parseInt(tokenizer.nextToken());
-                    list.get(item-1).markAsDone();
-                    System.out.println("    Nice! I've marked this task as done:");
-                    System.out.println("      [\u2713] " + list.get(item-1).description);
-                } else {
+            System.out.println("________________________________________");
+            switch (command) {
+                case "bye":
+                    exitCondition = true;
+                    System.out.println("    Bye. Hope to see you again soon!");
+                    break;
+                case "list":
+                    System.out.println("    Here are the tasks in your list:");
+                    for (int i = 0; i < items; i++) {
+                        System.out.println("    " + (i + 1) + ". " + list[i].toString());
+                    }
+                    break;
+                case "todo":
+                    list[items] = new Todo(description.substring(1));
+                    System.out.println("    Got it. I've added this task:\n" + "      "
+                            + list[items].toString());
                     items++;
-                    Task task = new Task(string);
-                    list.add(task);
-                    System.out.println("    added: "+string);
-                }
+                    System.out.println("    Now you have " + items + " task(s) in the list.");
+                    break;
+                case "deadline":
+                    list[items] = new Deadline(description.substring(1), date.substring(4));
+                    System.out.println("    Got it. I've added this task:\n" + "      "
+                            + list[items].toString());
+                    items++;
+                    System.out.println("    Now you have " + items + " task(s) in the list.");
+                    break;
+                case "event":
+                    list[items] = new Event(description.substring(1), date.substring(4));
+                    System.out.println("    Got it. I've added this task:\n" + "      "
+                            + list[items].toString());
+                    items++;
+                    System.out.println("    Now you have " + items + " task(s) in the list.");
+                    break;
+                case "done":
+                    int item = Integer.parseInt(description.substring(1));
+                    list[item - 1].markAsDone();
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println("      [\u2713] " + list[item - 1].description);
+                    break;
             }
             System.out.println("________________________________________");
         }
